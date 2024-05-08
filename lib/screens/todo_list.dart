f73c41cf4af6db93167953ec6f94c0c16494b75b
Masood ahmad <62877179+masood313@users.noreply.github.com>
2024-05-08 13:57:57 -0700

    Update todo_list.dart

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -13,6 +13,7 @@ class TodoListPage extends StatefulWidget {
 
 class _TodoListPageState extends State<TodoListPage> {
   List items = [];
+  bool isLoading = true;
   @override
   void initState() {
     super.initState();
@@ -33,37 +34,114 @@ class _TodoListPageState extends State<TodoListPage> {
             Colors.grey[900], // Set app bar color to match background
         centerTitle: true, // Center align the title
       ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = items[index] as Map;
-          return ListTile(
-            title: Text(item['title']),
-          );
-        },
+
+      body: Visibility(
+        visible: isLoading,
+        child: Center(child: CircularProgressIndicator()),
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index] as Map;
+              final id = item['_id'] as String;
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                  child: Text(
+                    '${index + 1}',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                title: Text(
+                  item['title'],
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  item['description'],
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: PopupMenuButton(
+                  onSelected: (value) {
+                    if (value == 'Edit') {
+                      navigateToEditPage(item);
+                    } else if (value == 'Delete') {
+                      deleteById(id);
+                    }
+                  },
+                  itemBuilder: (context) {
+                    return [
+                      PopupMenuItem(
+                        child: Text('Edit'),
+                        value: 'Edit',
+                      ),
+                      PopupMenuItem(
+                        child: Text('Delete'),
+                        value: 'Delete',
+                      ),
+                    ];
+                  },
+                ),
+              );
+            },
+          ),
+        ),
       ),
+
       floatingActionButton: FloatingActionButton.extended(
           onPressed: navigateToAddPage, label: Text('Add Todo')),
     );
   }
 
-  void navigateToAddPage() {
+  Future<void> navigateToEditPage(Map item) async {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(todo: item),
+    );
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  Future<void> navigateToAddPage() async {
     final route = MaterialPageRoute(
       builder: (context) => AddTodoPage(),
     );
-    Navigator.push(context, route);
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
   }
 
   Future<void> fetchTodo() async {
     final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
-    if (response.statusCode == 201) {
+    if (response.statusCode == 200) {
       final json = jsonDecode(response.body) as Map;
       final result = json['items'] as List;
       setState(() {
         items = result;
       });
-    } else {}
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> deleteById(String id) async {
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.delete(uri);
+    if (response.statusCode == 200) {
+      final filtered = items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filtered;
+      });
+    } else {
+      print('delete Failed');
+    }
   }
 }
